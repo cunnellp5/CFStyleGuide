32187dd163631975919c18f8d5d2372064d9654d
mfrazie2 <michael.s.frazier@gmail.com>
2018-08-30 20:48:57 -0600

    :sparkles:Add back button to main header
    * Add subscription in AppComponent to listen for changes in router path
    * Pass path to HeaderComponent to render back/smile icon buttons
    * Update protractor version in package.json (npm said it was a security risk)
    * Add padding to buttons page (first pass)
    
    :sparkles:

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
+import { Router, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+
+  _currentRoute = '/';
+  constructor(
+    private _location: Location,
+    private _router: Router
+  ) {}
+
+  ngOnInit() {
+    this._router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe(() => {
+      this._currentRoute = this._location.path();
+    })
+
+  }
 }
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,20 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, OnChanges } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
-
+export class HeaderComponent implements OnInit, OnChanges {
+  @Input() currentRoute: string;
   title: string;
   icon: string;
+  showBack = false;
 
   constructor() { }
 
   ngOnInit() {
     this.title = 'CF Style Guide';
     this.icon = 'insert_emoticon'; // keyboard_backspace for back
+  }
+
+  ngOnChanges(changes) {
+    if (changes.currentRoute) {
+      this.showBack = this.currentRoute !== '';
+    }
   }
 
 }
